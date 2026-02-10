6a4582619fc8a9305f94d2b371e3dff7ff46f69d
u <u@example.org>
2026-02-10 19:19:22 +0400

    Log record to string function

--- src/Record.php
+++ src/Record.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace LogParser;
 
-final readonly class Record
+final readonly class Record implements \Stringable
 {
     public function __construct(
         public \DateTimeImmutable $date,
@@ -12,4 +12,9 @@ final readonly class Record
         public int $length,
         public string $record,
     ) {}
+
+    public function __toString(): string
+    {
+        return \sprintf('%s %s', $this->date->format('Y-m-d H:i:s'), $this->record);
+    }
 }
---
+++ tests/RecordTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LogParserTests;
+
+use LogParser\Record;
+use PHPUnit\Framework\TestCase;
+use Carbon\CarbonImmutable;
+
+/**
+ * @internal
+ */
+final class RecordTest extends TestCase
+{
+    public function testToString(): void
+    {
+        $record = new Record(CarbonImmutable::parse('2001-02-03 04:05:06'), 7, 8, '9');
+        $this->assertSame('2001-02-03 04:05:06 9', (string) $record);
+    }
+}
