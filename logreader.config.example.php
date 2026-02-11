2246ba219bea68d65d1a900c6611e4a4bad27782
u <u@example.org>
2026-02-11 17:31:06 +0400

    Example config

---
+++ logreader.config.example.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use LogParser\LogReaderConfig;
+use LogParser\LogReaderConfigFile;
+use LogParser\Record;
+
+return new LogReaderConfig(
+    date: '2026-02-11 12:49:07',
+    timezone: 'UTC',
+    fileList: [
+        new LogReaderConfigFile(
+            filePath: 'example.log',
+            datePattern: '~\[\S+ (?<month>\S+) (?<day>\d{2}) (?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2}) (?<year>\d{4})]~',
+            filterFunction: static fn(Record $record): bool => 1 !== preg_match('~Accepted|Closing~', $record->record),
+        ),
+    ],
+);
