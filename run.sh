7d7749e32b553398c80d1db619d1a86a06d4ad23
u <u@example.org>
2026-02-13 10:34:45 +0400

    Run script for git hooks

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
 set -e
+
 test -n "$(docker image ls --quiet logreader:local)" || docker build --file configs/dockerfile --tag logreader:local .
-docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local "$@" && echo OK || echo FAIL
+
+test "$1" = '--script'\
+&& docker run --rm --volume "$PWD:/app:rw" --workdir /app logreader:local "${@:2}"\
+|| (docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local "$@" && echo OK || echo FAIL)
