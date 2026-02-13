26aa691e1cde8f2108dd164a91a7ac51542b25a8
u <u@example.org>
2026-02-13 09:51:58 +0400

    Build image in run script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+test -n "$(docker image ls --quiet logreader:local)" || docker build --file configs/dockerfile --tag logreader:local .
 docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local "$@" && echo OK || echo FAIL
