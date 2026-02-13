a91dfe4c4a8701b643665f73c5183248ab24c2a0
u <u@example.org>
2026-02-13 12:24:07 +0400

    Simplified archive for PHP 5.6

--- build.sh
+++ build.sh
@@ -15,13 +15,13 @@ cat src/* logreader.php\
 cat logreader.config.example.php\
 | grep -Fv declare\
 | grep -Fv use\
-> build/logreader56/logreader56.config.example.php
+> build/logreader56/logreader56.config.php
 
 docker build --file configs/dockerfile --tag logreader:local .
 docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local composer install
 cat configs/rector.php | sed -E 's~withPhpSets\(\)~withDowngradeSets\(php71:true\)~' > build/rector71.php
 docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local composer exec rector -- --config build/rector71.php build/logreader56/logreader56.php
-docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local composer exec rector -- --config build/rector71.php build/logreader56/logreader56.config.example.php
+docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local composer exec rector -- --config build/rector71.php build/logreader56/logreader56.config.php
 rm build/rector71.php
 
 cat build/logreader56/logreader56.php\
@@ -58,17 +58,17 @@ cat build/logreader56/logreader56.php\
 rm build/logreader56/logreader56.php
 mv build/logreader56/logreader56.tmp build/logreader56/logreader56.php
 
-cat build/logreader56/logreader56.config.example.php\
+cat build/logreader56/logreader56.config.php\
 | sed -E 's~: bool~~'\
 | sed -E 's~\w+: ~~'\
-> build/logreader56/logreader56.config.example.tmp
-rm build/logreader56/logreader56.config.example.php
-mv build/logreader56/logreader56.config.example.tmp build/logreader56/logreader56.config.example.php
+> build/logreader56/logreader56.config.tmp
+rm build/logreader56/logreader56.config.php
+mv build/logreader56/logreader56.config.tmp build/logreader56/logreader56.config.php
 
 cp example.log build/logreader56/example.log
 
 rm -f build/logreader56.tar.gz
 
-test "$1" = '--archive' && tar --create --directory build --file build/logreader56.tar.gz --gzip logreader56 && rm -r build/logreader56
+test "$1" = '--archive' && rm build/logreader56/example.log && tar --create --directory build/logreader56 --file build/logreader56.tar.gz --gzip logreader56.config.php logreader56.php && rm -r build/logreader56
 
 docker build --file configs/dockerfile56 --tag logreader56:local .
