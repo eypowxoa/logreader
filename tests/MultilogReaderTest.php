b2f2ef37a5cc5512d3fc7140f74890cca3e6d8fc
u <u@example.org>
2026-02-13 15:41:06 +0400

    Multilog period test cases

--- tests/MultilogReaderTest.php
+++ tests/MultilogReaderTest.php
@@ -9,6 +9,7 @@ use LogReader\LogReaderConfig;
 use LogReader\LogReaderConfigFile;
 use LogReader\MultilogPeriod;
 use LogReader\MultilogReader;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -16,12 +17,16 @@ use PHPUnit\Framework\TestCase;
  */
 final class MultilogReaderTest extends TestCase
 {
-    public function testReadConfigured(): void
+    /**
+     * @param string[] $expected
+     */
+    #[DataProvider('provideReadConfiguredCases')]
+    public function testReadConfigured(string $now, MultilogPeriod $multilogPeriod, array $expected): void
     {
         $recordList = [];
 
         $logReaderConfig = new LogReaderConfig(
-            '0001-01-01 00:01:00',
+            $now,
             'UTC',
             '',
             '',
@@ -41,10 +46,26 @@ final class MultilogReaderTest extends TestCase
         $fileReaderMemoryFactory = new FileReaderMemoryFactory();
         $multilogReader = new MultilogReader($fileReaderMemoryFactory);
 
-        foreach ($multilogReader->readConfigured($logReaderConfig, MultilogPeriod::MINUTE) as $record) {
+        foreach ($multilogReader->readConfigured($logReaderConfig, $multilogPeriod) as $record) {
             $recordList[] = $record->record;
         }
 
-        $this->assertSame(['d', 'a'], $recordList);
+        $this->assertSame($expected, $recordList);
+    }
+
+    /**
+     * @return iterable<string,array{string,MultilogPeriod,string[]}>
+     */
+    public static function provideReadConfiguredCases(): iterable
+    {
+        yield 'should find for minute' => ['0001-01-01 00:01:04', MultilogPeriod::MINUTE, ['e', 'b']];
+
+        yield 'should find for hour' => ['0001-01-01 01:00:04', MultilogPeriod::HOUR, ['e', 'b']];
+
+        yield 'should find for day' => ['0001-01-02 00:00:04', MultilogPeriod::DAY, ['e', 'b']];
+
+        yield 'should find for week' => ['0001-01-08 00:00:04', MultilogPeriod::WEEK, ['e', 'b']];
+
+        yield 'should find for month' => ['0001-02-01 00:00:04', MultilogPeriod::MONTH, ['e', 'b']];
     }
 }
