91eee27100e8d8e26130b24b756066365aef87d5
u <u@example.org>
2026-03-16 12:00:43 +0400

    Refactoring

--- tests/RecordTest.php
+++ tests/RecordTest.php
@@ -15,8 +15,13 @@ final class RecordTest extends TestCase
 {
     public function testConstruct(): void
     {
-        $record = new Record(CarbonImmutable::parse('2001-02-03 04:05:06'), 7, 8, '9');
+        $date = CarbonImmutable::parse('2001-02-03 04:05:06');
+        $record = new Record($date, 7, 8, '9');
+        $this->assertSame($date, $record->date);
+        $this->assertSame(8, $record->length);
+        $this->assertSame(7, $record->position);
         $this->assertSame(15, $record->border);
+        $this->assertSame('9', $record->record);
     }
 
     public function testToString(): void
