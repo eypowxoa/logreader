7ac933ec2929062baefffc021a059e2eb90fe31c
u <u@example.org>
2026-04-17 13:34:38 +0400

    Fix git hook do not stop on fail

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ set -e
 
 test -n "$(docker image ls --quiet logreader:local)" || docker build --file configs/dockerfile --tag logreader:local .
 
-test "$1" = '--script'\
-&& docker run --rm --volume "$PWD:/app:rw" --workdir /app logreader:local "${@:2}"\
-|| (docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local "$@" && echo OK || echo FAIL)
+if test "$1" = '--script'; then
+    docker run --rm --volume "$PWD:/app:rw" --workdir /app logreader:local "${@:2}"
+else
+    docker run --interactive --rm --tty --volume "$PWD:/app:rw" --workdir /app logreader:local "$@" && echo OK
+fi
